150e06bf94dc8e1905cd183d1b530fb6dc20a717
Lalit Chaudhari <lalit.chaudhari0@gmail.com>
2022-08-30 20:02:43 +0530

    teacher model added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    number = models.IntegerField()
+    class Meta:
+        db_table ='Teacher'
